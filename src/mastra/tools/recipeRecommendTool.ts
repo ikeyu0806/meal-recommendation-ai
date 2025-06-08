a6e8fa83b8d70fe3837130842438deb73402aa01
ikeyu0806 <ikeyu0806@gmail.com>
2025-06-08 10:14:19 +0900

    fix recipeRecommendTool.ts

--- src/mastra/tools/recipeRecommendTool.ts
+++ src/mastra/tools/recipeRecommendTool.ts
@@ -5,8 +5,11 @@ import { dirname, join } from 'path'
 import { createTool } from '@mastra/core/tools'
 import { z } from 'zod'
 
+// .mastra/output/toolsに出力されたファイルからの相対でレシピのテキストファイルを読み込むためのパスを設定
+// rootDirの取得など強引な気もするが一旦これで動かす
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+const rootDir = __dirname.replace(/\/?\.mastra\/output\/tools\/?$/, '')
 
 export const recipeRecommendTool = createTool({
   id: 'Get Recipe Information',
@@ -15,7 +18,7 @@ export const recipeRecommendTool = createTool({
   }),
   description: `Fetches the Recipe information from a file`,
   execute: async () => {
-    const filePath = join(__dirname, '../../src/mastra/texts/recipe.txt')
+    const filePath = join(rootDir, 'src/mastra/texts/recipe.txt')
     const contents = fs.readFileSync(filePath, 'utf8')
 
     return {
