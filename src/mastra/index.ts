c4654bd924725113ddb244d9809d502cf6c9c042
ikeyu0806 <ikeyu0806@gmail.com>
2025-06-08 09:31:55 +0900

    add .prettierrc.json

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,17 +1,17 @@
-import { Mastra } from "@mastra/core/mastra";
-import { PinoLogger } from "@mastra/loggers";
-import { LibSQLStore } from "@mastra/libsql";
+import { Mastra } from '@mastra/core/mastra'
+import { PinoLogger } from '@mastra/loggers'
+import { LibSQLStore } from '@mastra/libsql'
 
-import { weatherAgent } from "./agents/weather-agent";
+import { weatherAgent } from './agents/weather-agent'
 
 export const mastra = new Mastra({
   agents: { weatherAgent },
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
+    url: ':memory:',
   }),
   logger: new PinoLogger({
-    name: "Mastra",
-    level: "info",
+    name: 'Mastra',
+    level: 'info',
   }),
-});
+})
